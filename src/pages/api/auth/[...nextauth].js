d099e21946d61903feafb99fce4f835a342885cd
rzere <rzere@rollins.edu>
2024-04-07 18:19:53 +0300

    prisma

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,14 +1,5 @@
-import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import prisma from '@/lib/prisma'; // Import the existing Prisma instance
+import { PrismaClient } from '@prisma/client';
 
-export default NextAuth({
-  adapter: PrismaAdapter(prisma), // Use the imported Prisma instance
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-});
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;

6a14d00090dca752677a83547832d065a90876de
rzere <rzere@rollins.edu>
2024-04-07 18:27:33 +0300

    jojo fix 3

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,11 +1,7 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-import { PrismaClient } from '@prisma/client';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import prisma from '@/lib/prisma'; // Import the existing Prisma instance
-
-const prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma), // Use the imported Prisma instance

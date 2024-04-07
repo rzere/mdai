ccfe8ef42cee6bb04c3dd46e1189f6f4784c9b11
rzere <rzere@rollins.edu>
2024-04-07 17:58:13 +0300

    fix?

--- app/api/auth/[...nextauth].js
+++ app/api/auth/[...nextauth].js
@@ -1,10 +1,5 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
 
 export default NextAuth({
   providers: [
--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,6 +2,11 @@ import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import prisma from '@/lib/prisma';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),

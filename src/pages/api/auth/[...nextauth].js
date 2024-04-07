ac61d3cc87f8dcbcf7ebeb2803f836042ad0c2bb
rzere <rzere@rollins.edu>
2024-04-07 22:45:08 +0300

    remove prisma completely

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,10 +1,7 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import prisma from '@/lib/prisma'; // Import the existing Prisma instance
 
 export default NextAuth({
-  adapter: PrismaAdapter(prisma), // Use the imported Prisma instance
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,

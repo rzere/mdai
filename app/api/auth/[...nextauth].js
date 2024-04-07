5e6edfda1ab626f02d3dfa0630b78580772e15f8
rzere <rzere@rollins.edu>
2024-04-07 17:55:40 +0300

    prisma client

--- app/api/auth/[...nextauth].js
+++ app/api/auth/[...nextauth].js
@@ -1,5 +1,10 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
 
 export default NextAuth({
   providers: [

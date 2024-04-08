f5d02ef71942b7d31ede02da830867333258fb41
rzere <rzere@rollins.edu>
2024-04-08 14:39:41 +0300

    config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+module.exports = {
+    webpack: (config) => {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@': __dirname,
+      };
+      return config;
+    },
+  };

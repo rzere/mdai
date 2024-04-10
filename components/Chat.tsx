4f235176177399afb0bb7064f46d660638d076fe
rzere <rzere@rollins.edu>
2024-04-11 00:48:50 +0300

    pixel test

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -17,7 +17,7 @@ if (!session) {
       <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between w-full">
           <Link href="/" className="flex items-center font-white text-2xl">
           <img
-          src={`https://leadshark.vercel.app/_next/image?url=%2Flogo.png&w=64&q=75`}
+          src={`https://raw.githubusercontent.com/rzere/mdai/main/logo.jpeg`}
           alt="MD Logo"
         />
           </Link>
@@ -75,7 +75,7 @@ return (
       <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between w-full">
           <Link href="/" className="flex items-center font-white text-2xl">
           <img
-          src={`https://leadshark.vercel.app/_next/image?url=%2Flogo.png&w=64&q=75`}
+          src={`https://raw.githubusercontent.com/rzere/mdai/main/logo.jpeg`}
           alt="MD Logo"
         />
           </Link>
@@ -97,6 +97,10 @@ return (
         </header>
 
     {/* Chat Container */}
+    <img
+          src={`https://raw.githubusercontent.com/rzere/mdai/main/components/pixel-speech-bubble.png`}
+          alt="MD Logo"
+        />
     <main  className="container mx-auto px-4 py-8 flex-grow">
         <div className="relative mt-2 rounded-md shadow-sm">
         {messages.length > 0 ? (
